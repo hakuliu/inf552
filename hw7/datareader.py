1463fa74d2138753e25d0bec995a79921ff9e704
Paul An <hakuliu@gmail.com>
2017-04-27 22:26:49 -0700

    Added WEKA outputs.

--- hw7/datareader.py
+++ hw7/datareader.py
@@ -14,3 +14,14 @@ class DataPoint:
         self.x = x
         self.y = y
         self.val = val
+
+
+def writecsv(fname, data):
+    f = open(fname, 'w')
+    for d in data:
+        yesno = "yes"
+        if d.val < 0: yesno = "no"
+        f.write(str(d.x)+","+str(d.y)+","+yesno+"\n")
+
+data = readData('nonlinsep.txt')
+writecsv('nonlinsep.csv', data)
